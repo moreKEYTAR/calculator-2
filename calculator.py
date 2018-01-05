cb040b7314ec77c20871c668c3456b9093932311
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 16:30:55 -0800

    added functions; mod needs fixing

--- calculator.py
+++ calculator.py
@@ -29,6 +29,27 @@ while True:
             if i > 0:
                 tokens[i] = float(tokens[i])
 
-        # checks for add sign, then calls add function
+        # checks for math sign, then calls math function
         if tokens[0] == "+":
             print add(tokens[1], tokens[2])
+
+        elif tokens[0] == "-":
+            print subtract(tokens[1], tokens[2])
+
+        elif tokens[0] == "*":
+            print multiply(tokens[1], tokens[2])
+
+        elif tokens[0] == "/":
+            print divide(tokens[1], tokens[2])
+
+        elif tokens[0] == "square":
+            print square(tokens[1])
+
+        elif tokens[0] == "cube":
+            print cube(tokens[1])
+
+        elif tokens[0] == "pow":
+            print power(tokens[1], tokens[2])
+
+        elif tokens[0] == "mod":
+            print mod(tokens[1], tokens[2])
